175b41723f78604d43819b71460d9722f0416586
matancotton <matancotton1@gmaol.com>
2021-02-02 19:33:30 +0200

    Edit to ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,11 +4,14 @@ import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-toal';
 import numeral from 'numeral';
 
-export const ExpensesSummary = (props) => (
-    <div>
-        <p>Viewing {props.expenseCount} expenses totalling {numeral(props.expensesTotal / 100).format('$0,0.00')}</p>
-    </div>
-)
+export const ExpensesSummary = (props) => {
+    const expenseWord = props.expenseCount === 1 ? 'expense' : 'expenses';
+    return (
+        <div>
+            <h1>Viewing {props.expenseCount} {expenseWord} totalling {numeral(props.expensesTotal / 100).format('$0,0.00')}</h1>
+        </div>
+    )
+}
 
 const mapStateToProps = (state) => ({
     expenseCount: selectExpenses(state.expenses,state.filters).length,
